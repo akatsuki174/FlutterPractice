9c20b803fb500fae599b08fcadac515ec6daa6eb
akatsuki174 <dev2015js@gmail.com>
2020-06-02 23:50:33 +0900

    show simple text

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -1,6 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) => MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      primarySwatch: Colors.blue
+    ),
+    home: Scaffold(
+      body: Center(
+        child: Text(
+          'Flutter Demo Home Page',
+          style: Theme.of(context).textTheme.headline4,
+        ),
+      ),
+    ),
+  );
 }
